463747d360b600d106a515ee0e8a3ea19f2a5f43
avinash <avinash.paladugula@beautifulcode.in>
2025-10-01 15:53:18 +0530

    Added REST-endpoint module

---
+++ REST-Endpoints/go-jwt/middleware/requireAuth.go
@@ -0,0 +1,64 @@
+package middleware
+
+import (
+	"fmt"
+	"go-jwt/initializers"
+	"go-jwt/models"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func RequireAuth(c *gin.Context) {
+	// fmt.Println("In middleware") // this will be printed in console
+
+	// Get the cookie off req
+	tokenString, err := c.Cookie("Authorization")
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	// Decode/validate it
+
+	// Parse takes the token string and a function for looking up the key. The latter is especially
+	// useful if you use multiple keys for your application.  The standard is to use 'kid' in the
+	// head of the token to identify which key to use, but the parsed token (head and claims) is provided
+	// to the callback, providing flexibility.
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(os.Getenv("SECRET")), nil
+	}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		// Check the exp
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		// Find the user with token sub
+		var user models.User
+		initializers.DB.First(&user, claims["sub"])
+		if user.ID == 0 {
+			fmt.Println(".........................")
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		// Attach to req
+		c.Set("user", user)
+
+		// Continue
+		c.Next()
+
+		// fmt.Println(claims["foo"], claims["nbf"])
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+}
